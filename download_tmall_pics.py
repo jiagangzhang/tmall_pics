1b54f48b81f5b51c5d5beb4976eafa0e7193b4c4
Jiagang Zhang <jiagang.zhang@thebeastshop.com>
2020-11-05 16:07:44 +0800

    需求变更为取天猫商品详情图，将代码适配

--- download_tmall_pics.py
+++ download_tmall_pics.py
@@ -6,18 +6,24 @@ def get_image_urls(tmall_url: str) -> [str]:
 	resp = requests.get(tmall_url)
 	soup = BeautifulSoup(resp.content, 'html.parser')
 	img_url_list = []
-	imgs = soup.find_all('img', attrs={'aria-label':'商品主图'})
+	imgs = soup.find_all('img', attrs={'aria-label':'商品详情图'})
 	for i in imgs:
-		src = i['src']
+		src = i['data-ks-lazyload']
+		# src = i['src']
+		# if not (src.endswith('.jpg') or src.endswith('.webp')):
+		# 	try:
+		# 		src = i['data-src']
+		# 	except Exception as e:
+		# 		print('！！！！无法找到有效图片地址！！！！')
+		# 		print(f'当前<img> 为 {i}')
+		# 		raise e
 		if not (src.endswith('.jpg') or src.endswith('.webp')):
-			try:
-				src = i['data-src']
-			except Exception as e:
-				print('！！！！无法找到有效图片地址！！！！')
-				print(f'当前<img> 为 {i}')
-				raise e
+			print('！！！！无法找到有效图片地址！！！！')
+			print(f'当前<img> 为 {i}')
+			continue
 
-		http_url = 'http:' + str(src)
+		# http_url = 'http:' + str(src)
+		http_url = str(src)
 		if http_url not in img_url_list:
 			img_url_list.append(http_url)
 
